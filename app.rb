ee3ac2ca7b8e12be9cbbb16eb4fd14b590d318fa
NekoAtsume <wolvesofgrace@yahoo.com>
2018-07-31 18:17:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+   get '/' do
+    "My name is Della."
+  end
+  
+   get '/hometown' do
+    "My hometown is Indianapolis."
+  end
+  
+   get '/song' do
+    "My favorite song is Faded."
+  end
+  
+   get '/hi' do
+    "Hello, World!"
+  end
+  
 end
